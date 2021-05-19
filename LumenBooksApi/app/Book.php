1daf0bb62c6818f6e67e0db51c0a52432756fc7d
Thomas F Feijoo <thomwe8@hotmail.com>
2021-05-18 22:24:47 -0300

    Book model

--- LumenBooksApi/app/Book.php
+++ LumenBooksApi/app/Book.php
@@ -8,25 +8,17 @@ use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Lumen\Auth\Authorizable;
 
-class User extends Model implements AuthenticatableContract, AuthorizableContract
+class Book extends Model
 {
-    use Authenticatable, Authorizable;
-
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email',
-    ];
-
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
+        'title', 
+        'description',
+        'price',
+        'author_id'
     ];
 }
