708a31bd66e30f40c0283eef55fdcf6c00529d9a
Thomas F Feijoo <thomwe8@hotmail.com>
2021-05-18 22:35:37 -0300

    book controller

---
+++ LumenBooksApi/app/Http/Controllers/BookController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{    
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Return list of books
+     * @return Illuminate\Http\Response
+     */
+    public function index() {
+
+    }
+
+    /**
+     * Create new book
+     * @return Illuminate\Http\Response
+     */
+    public function store(Request $request) {
+
+    }
+
+    /**
+     * Obtain and show one book
+     * @return Illuminate\Http\Response
+     */
+    public function show($book) {
+
+    }
+
+    /**
+     * Update an existing book
+     * @return Illuminate\Http\Response
+     */
+    public function update(Request $request, $book) {
+
+    }
+
+    /**
+     * Remove an existing book
+     * @return Illuminate\Http\Response
+     */
+    public function destroy($book) {
+
+    }
+}
