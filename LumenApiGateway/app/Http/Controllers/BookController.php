d848a43fed4319236f4c5966a915adde5d8b9a50
Thomas F Feijoo <thomwe8@hotmail.com>
2021-05-24 19:53:21 -0300

    book controller to receive book service

--- LumenApiGateway/app/Http/Controllers/BookController.php
+++ LumenApiGateway/app/Http/Controllers/BookController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Book;
+use App\Services\BookService;
 use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -10,14 +11,21 @@ use Illuminate\Http\Response;
 class BookController extends Controller
 {
     use ApiResponser;    
+    
+    /**
+     * The service to consume the books microservice
+     * @var BookService
+     */
+    public $bookService;
+    
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(BookService $bookService)
     {
-        //
+        $this->bookService = $bookService;
     }
 
     /**
