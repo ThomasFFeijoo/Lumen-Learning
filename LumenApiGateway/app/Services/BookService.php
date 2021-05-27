5da634c470b6afb0791eccaaf377405d1da2963f
Thomas F Feijoo <thomwe8@hotmail.com>
2021-05-27 19:29:21 -0300

    new book service, based on author service

--- LumenApiGateway/app/Services/BookService.php
+++ LumenApiGateway/app/Services/BookService.php
@@ -18,4 +18,49 @@ class BookService
     {
         $this->baseUri = config('services.books.base_uri');
     }
+
+    /**
+     * Obtain the full list of books from the book service
+     * @return string
+     */
+    public function obtainBooks()
+    {
+        return $this->performRequest('GET', '/books');
+    }
+
+    /**
+     * Create one book using the book service
+     * @return string
+     */
+    public function createBook($data)
+    {
+        return $this->performRequest('POST', '/books', $data);
+    }
+
+    /**
+     * Obtain book from the book service
+     * @return string
+     */
+    public function obtainBook($book)
+    {
+        return $this->performRequest('GET', "/books/{$book}");
+    }
+
+    /**
+     * Update an book from the book service
+     * @return string
+     */
+    public function editBook($data, $book)
+    {
+        return $this->performRequest('PUT', "/books/{$book}", $data);
+    }
+
+    /**
+     * Delete book from the book service
+     * @return string
+     */
+    public function deleteBook($book)
+    {
+        return $this->performRequest('DELETE', "/books/{$book}");
+    }
 }
