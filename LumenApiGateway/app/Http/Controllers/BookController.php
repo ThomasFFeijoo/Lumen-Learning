073c073a019fbd86c7e90e2fe2e651413430dd12
Thomas F Feijoo <thomwe8@hotmail.com>
2021-05-28 22:37:55 -0300

    check author before create book

--- LumenApiGateway/app/Http/Controllers/BookController.php
+++ LumenApiGateway/app/Http/Controllers/BookController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Book;
+use App\Services\AuthorService;
 use App\Services\BookService;
 use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
@@ -17,15 +17,22 @@ class BookController extends Controller
      * @var BookService
      */
     public $bookService;
+
+    /**
+     * The service to consume the authors microservice
+     * @var AuthorService
+     */
+    public $authorService;
     
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct(BookService $bookService)
+    public function __construct(BookService $bookService, AuthorService $authorService)
     {
         $this->bookService = $bookService;
+        $this->authorService = $authorService;
     }
 
     /**
@@ -43,6 +50,7 @@ class BookController extends Controller
      */
     public function store(Request $request) 
     {
+        $this->authorService->obtainAuthor($request->author_id);
         return $this->successResponse($this->bookService->createBook($request->all(), Response::HTTP_CREATED));
     }
 
